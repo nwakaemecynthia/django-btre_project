6b4d7c81e19b6bf5b4c8ea68bab2a88bafcc59a6
nwakaemecynthia <nwakaemecynthia@gmail.com>
2024-12-28 14:47:29 +0100

    Data mutation via django adnin

--- btre/settings.py
+++ btre/settings.py
@@ -135,3 +135,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
