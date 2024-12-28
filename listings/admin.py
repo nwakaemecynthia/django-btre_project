e6c703bf59075cd915a125b938bf830162d9c895
nwakaemecynthia <nwakaemecynthia@gmail.com>
2024-12-28 18:18:33 +0100

    customizing Django admin table data display

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Listing
 
-# Register your models here.
-
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor') #table columns
+    list_display_links = ('id', 'title') # clickable columns to help navigate to the detail page
+    list_filter = ('realtor',) #aside filter (this is not the table header filter as the heder filter is default for all table header)
+    list_editable = ('is_published',) #allows you mutate data from the table afterwards you have to save to effectively update your changes
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price') #search by
+    #take nothe that when a parameter has only one value a comma follows, this is to help ensure your code doesn't run into error 
+    list_per_page = 25 #this defines the amount of listings per page (PAGINATION)
+    
+ 
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Realtor
 
-# Register your models here.
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25 #this defines the amount of listings per page (PAGINATION)
+
+    
+admin.site.register(Realtor, RealtorAdmin)
