6e0bfbd36e2cdcbd7cbc01ff2325e78b7be5acc8
nwakaemecynthia <nwakaemecynthia@gmail.com>
2024-12-31 02:33:42 +0100

    search and filter from DB

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
+from .emun import bedroom_choices, price_choices, state_choices
+
 
 # Create your views here.
 
@@ -24,4 +26,42 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', {'listing': listing})
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    
+    #Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+            
+    #City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city) #`__iexact` allows for case insensitivity but `__exact` is case sensitive
+    
+    #State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state) 
+            
+    #Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)#when a search is 4; lte implies: get all beaddrooms from 4 below (Less Than Equalto) 
+    #Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+    
+    data = {
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', data)
--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from realtors.models import Realtor
 from listings.models import Listing
 
+from listings.emun import bedroom_choices, price_choices, state_choices
+
 
 # Create your views here.
 
@@ -12,13 +14,19 @@ from listings.models import Listing
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-    return render(request, 'pages/index.html', {'listings': listings})
+    data = {
+        'listings': listings,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+    }
+    return render(request, 'pages/index.html', data)
 
 def about(request):
     realtors = Realtor.objects.order_by('-is_mvp', '-hire_date')
     mvp_realtor = Realtor.objects.filter(is_mvp=True)
-    context = {'realtors' : realtors, 'mvp_realtor': mvp_realtor}
-    return render(request, 'pages/about.html', context)
+    data = {'realtors' : realtors, 'mvp_realtor': mvp_realtor}
+    return render(request, 'pages/about.html', data)
 
 def login(request):
     return render(request, 'pages/login.html')
