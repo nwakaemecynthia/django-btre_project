9f176130d8709dcd0ec3f9225a3ef922f544a960
nwakaemecynthia <nwakaemecynthia@gmail.com>
2024-12-31 01:09:23 +0100

    fetch a single data and filter from db using models

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
@@ -10,22 +10,18 @@ def index(request):
     #     'name': 'Brad'
     # })
     
-    # listings = Listing.objects.all()
+    # listings = Listing.objects.all() #Fetch all data from DB
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
-    
-    #Paginate the data where 6 is the page size (total number of data per page)
-    paginator = Paginator(listings, 6)
+    paginator = Paginator(listings, 6)  #Paginate the data where 6 is the page size (total number of data per page)
     page = request.GET.get("page")
     paged_listings = paginator.get_page(page)
 
-
-    
-    context = {'listings' : paged_listings}
-    return render(request, 'listings/listings.html', context)
+    return render(request, 'listings/listings.html', {'listings': paged_listings})
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    return render(request, 'listings/listing.html', {'listing': listing})
 
 def search(request):
     return render(request, 'listings/search.html')
