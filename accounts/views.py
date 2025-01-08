ff1d04311c40aae5bf8378c24c133790cfcb95ed
nwakaemecynthia <nwakaemecynthia@gmail.com>
2025-01-08 14:56:43 +0100

    authentication - login, register and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def login(request):
     if request.method == "POST":
-        messages.error(request, 'Testing error messaging')
-        return redirect('signin')
+        # Get form values
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are logged in')
+            return redirect('dashboard')
+        else:
+           messages.error(request, 'Invalid Credentials') 
+           return redirect('signin')
     else:
         return render(request, 'accounts/login.html')
 
@@ -23,7 +34,26 @@ def register(request):
         # Validation
         # Check if passwords match
         if password == password2:
-            return
+            # Check if username exist
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('signup')
+            else:
+                # Check if email exist
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email already exist')
+                    return redirect('signup')
+                else:
+                    # Looks Good
+                    user = User.objects.create_user(username=username, email=email, first_name=first_name, last_name=last_name, password=password)
+                    # Login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')
+                    
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('signin')
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('signup')
@@ -31,8 +61,12 @@ def register(request):
     else:
         return render(request, 'accounts/register.html')
 
-def logout():
-    return redirect('index')
+def logout(request):
+    if request.method == "POST":
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
+        
 
 def dashboard(request):
-    return render(request, 'accounts/register.html')
+    return render(request, 'accounts/dashboard.html')
