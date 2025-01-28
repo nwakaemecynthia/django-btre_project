a7fdac069a908fc12b74a50ce0b7aa45831c1529
nwakaemecynthia <nwakaemecynthia@gmail.com>
2025-01-28 23:18:27 +0100

    updated learnings and added requirements file

--- btre/settings.py
+++ btre/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6ok@v5@pw#t*livdt3)!^eb_n628*x#htlvx(lhqhz2^_+@9+b'
+SECRET_KEY = config('SECRET_KEY', default='babyShark')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
